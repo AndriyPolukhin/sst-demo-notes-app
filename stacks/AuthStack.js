36976edb2cd9393a37dcebcbb76e7213424900c6
Andriy Polukhin <andriy.polukhin@gmail.com>
2022-08-17 19:28:34 +0300

    Builded the React app

--- stacks/AuthStack.js
+++ stacks/AuthStack.js
@@ -1,40 +1,40 @@
-import * as iam from 'aws-cdk-lib/aws-iam'
-import { Auth, use } from '@serverless-stack/resources'
-import { StorageStack } from './StorageStack'
-import { ApiStack } from './ApiStack'
+import * as iam from "aws-cdk-lib/aws-iam";
+import { Auth, use } from "@serverless-stack/resources";
+import { StorageStack } from "./StorageStack";
+import { ApiStack } from "./ApiStack";
 
 export function AuthStack({ stack, app }) {
-	const { bucket } = use(StorageStack)
-	const { api } = use(ApiStack)
+	const { bucket } = use(StorageStack);
+	const { api } = use(ApiStack);
 
 	// Create a Cognito User Pool and Identity Pool
-	const auth = new Auth(stack, 'Auth', {
-		login: ['email']
-	})
+	const auth = new Auth(stack, "Auth", {
+		login: ["email"],
+	});
 
 	auth.attachPermissionsForAuthUsers(stack, [
 		// Allow access to the API
 		api,
 		// Policy granting access to a specific folder in the bucket
 		new iam.PolicyStatement({
-			actions: ['s3:*'],
+			actions: ["s3:*"],
 			effect: iam.Effect.ALLOW,
 			resources: [
-				bucket.bucketArn + '/private/${cognito-identity.amazonaws.com:sub}/*'
-			]
-		})
-	])
+				bucket.bucketArn + "/private/${cognito-identity.amazonaws.com:sub}/*",
+			],
+		}),
+	]);
 
 	// Show the auth resources in the output
 	stack.addOutputs({
 		Region: app.region,
 		UserPoolId: auth.userPoolId,
 		IdentityPoolId: auth.cognitoIdentityPoolId,
-		UserPoolClientId: auth.userPoolClientId
-	})
+		UserPoolClientId: auth.userPoolClientId,
+	});
 
 	// Return the auth resource
 	return {
-		auth
-	}
+		auth,
+	};
 }
--- stacks/index.js
+++ stacks/index.js
@@ -1,22 +1,36 @@
 // App Infrastructure
 
-import { App } from '@serverless-stack/resources'
-import { StorageStack } from './StorageStack'
-import { ApiStack } from './ApiStack'
-import { AuthStack } from './AuthStack'
-import { FrontendStack } from './FrontendStack'
+import { App } from "@serverless-stack/resources";
+import { StorageStack } from "./StorageStack";
+import { ApiStack } from "./ApiStack";
+import { AuthStack } from "./AuthStack";
+import { FrontendStack } from "./FrontendStack";
 
 /**
  * @param {App} app
  */
 export default function main(app) {
 	app.setDefaultFunctionProps({
-		runtime: 'nodejs16.x',
-		srcPath: 'services',
+		runtime: "nodejs16.x",
+		srcPath: "services",
 		bundle: {
-			format: 'esm'
-		}
-	})
+			format: "esm",
+		},
+	});
+
+	/**
+	 * Adds additional default environment variables to be applied to all Lambda functions in the app.
+		addDefaultFunctionEnv(environment)
+
+
+	* Adds a default function permission
+		app.addDefaultFunctionPermissions({
+			"MY_ENV_VAR": "my-value"
+		})
+	*/
+
+	// Default removal policy
+	app.setDefaultRemovalPolicy(app.local ? "destroy" : "retain");
 	// Here we concatenate stacks
-	app.stack(StorageStack).stack(ApiStack).stack(AuthStack).stack(FrontendStack)
+	app.stack(StorageStack).stack(ApiStack).stack(AuthStack).stack(FrontendStack);
 }
